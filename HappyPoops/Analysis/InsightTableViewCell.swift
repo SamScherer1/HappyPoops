32526953b19e56367d834fd22ff62afb34b2c667
SamScherer1 <samscherer33@gmail.com>
2021-06-30 18:02:22 -0400

    start working on resizable insight cells

--- HappyPoops/Analysis/InsightTableViewCell.swift
+++ HappyPoops/Analysis/InsightTableViewCell.swift
@@ -11,10 +11,12 @@ import UIKit
 
 class InsightTableViewCell: UITableViewCell {
     
-    var arrowView : UIImageView!
-    var titleLabel : UILabel!
+    var arrowView = UIImageView.init(image: UIImage.init(systemName: "chevron.right.circle"))
+    var titleLabel = UILabel()
+    var insightsLabel = UILabel()
     
     var cellHeightConstraint : NSLayoutConstraint?
+    var insightsHeightConstraint : NSLayoutConstraint?
     
     var arrowInitialized = false
     
@@ -29,10 +31,9 @@ class InsightTableViewCell: UITableViewCell {
     }
     
     func setupView() {
-        self.cellHeightConstraint = self.contentView.heightAnchor.constraint(equalToConstant: 60.0)
-        self.cellHeightConstraint?.isActive = false
+        self.cellHeightConstraint = self.contentView.heightAnchor.constraint(equalToConstant: 30.0)
+        self.cellHeightConstraint?.isActive = true
         
-        self.arrowView = UIImageView.init(image: UIImage.init(systemName: "chevron.right.circle"))
         self.arrowView.translatesAutoresizingMaskIntoConstraints = false
         self.contentView.addSubview(self.arrowView)
         self.arrowView.rightAnchor.constraint(equalTo: self.contentView.rightAnchor, constant: -5.0).isActive = true
@@ -40,7 +41,6 @@ class InsightTableViewCell: UITableViewCell {
         self.arrowView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -5.0).isActive = true
         self.arrowView.widthAnchor.constraint(equalTo: self.arrowView.heightAnchor).isActive = true
         
-        self.titleLabel = UILabel.init()
         self.titleLabel.translatesAutoresizingMaskIntoConstraints = false
         self.contentView.addSubview(self.titleLabel)
         self.titleLabel.leftAnchor.constraint(equalTo: self.contentView.leftAnchor, constant: 15.0).isActive = true
@@ -49,6 +49,15 @@ class InsightTableViewCell: UITableViewCell {
         self.titleLabel.text = "Food Type Title"
         self.titleLabel.textColor = .white
         self.selectionStyle = .none
+
+        self.insightsLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.contentView.addSubview(self.insightsLabel)
+        self.insightsLabel.textColor = .white
+        self.insightsLabel.text = "TEST TEXT"
+        self.insightsLabel.leftAnchor.constraint(equalTo: self.titleLabel.leftAnchor, constant: 5.0).isActive = true
+        self.insightsLabel.topAnchor.constraint(equalTo: self.titleLabel.bottomAnchor, constant: 5.0).isActive = true
+        self.insightsHeightConstraint = self.insightsLabel.heightAnchor.constraint(equalToConstant: 0)
+        self.insightsHeightConstraint?.isActive = true
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -71,10 +80,9 @@ class InsightTableViewCell: UITableViewCell {
             self.arrowView.transform = self.arrowView.transform.rotated(by: .pi/2.0)
         })
 
-        //Increase cell size
-        
-        
-        //Show content
+        //Increase cell size/ show content
+        self.cellHeightConstraint?.constant = 100.0
+        self.insightsHeightConstraint?.constant = 70.0
     }
     
     func hideDetails() {
@@ -83,8 +91,8 @@ class InsightTableViewCell: UITableViewCell {
             self.arrowView.transform = self.arrowView.transform.rotated(by: -1 * .pi/2.0)
         }
 
-        //Decrease cell size
-        
-        //Hide content
+        //Decrease cell size/ show content
+        self.cellHeightConstraint?.constant = 30.0
+        self.insightsHeightConstraint?.constant = 0.0
     }
 }
